7888e6d182da1457ef4da2dd62fc5e19fe78b2e5
climba <mike@joomla-go.com>
2018-04-30 22:39:16 -0700

    Added new file and all the questions for the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,92 @@
+const myQuestions = [
+  {
+    question: "In rhyming slang, if someone said they we going to have a 'bo-peep', what they be doing?",
+    answers: {
+      a: "Having a sleep",
+      b: "Have a wee",
+      c: "Drink some beer",
+      d: "Cook a BBQ"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Australians hold the world record for which amazing feat?",
+    answers: {
+      a: "Biggest BBQ",
+      b: "Largest Christmas Cracker",
+      c: "Most beer drank",
+      d: "Biggest didgeridoo"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What is Kylie Minogue’s lingerie line called?",
+    answers: {
+      a: "Kylie Love",
+      b: "Love from Kylie",
+      c: "Love Kylie",
+      d: "Kylie"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Where is the Big Prawn?",
+    answers: {
+      a: "Ballina",
+      b: "Batemans Bay",
+      c: "Bateau Bay",
+      d: "Baudin Beach"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Approximately how many rabbits are there in Australia today?",
+    answers: {
+      a: "3 million",
+      b: "30 million",
+      c: "300 million",
+      d: "3000 million"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What was the name of Olivia Newton-John’s chain of clothing stores?",
+    answers: {
+      a: "Xanadu",
+      b: "Koala Blue",
+      c: "Oliva",
+      d: "Liv"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What famous beach is known for being in the final scene of point break?",
+    answers: {
+      a: "Bells Beach",
+      b: "Bedourie",
+      c: "Byron Bay",
+      d: "Beachlands"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which of the following is not an Australian innovation?",
+    answers: {
+      a: "Chiko Roll",
+      b: "Lawn Sprinkler",
+      c: "Plastic bank notes",
+      d: "Wine Cask"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What creature does Jana Pittman have tattooed on her body?",
+    answers: {
+      a: "Scorpion",
+      b: "Bee",
+      c: "Dragon",
+      d: "Owl"
+    },
+    correctAnswer: "b"
+  }
+];
