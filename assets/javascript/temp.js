8e592485a265e9dc83d03aba1730691ed8383072
climba <mike@joomla-go.com>
2018-05-07 18:26:25 -0700

    Tweaked some text

---
+++ assets/javascript/temp.js
@@ -0,0 +1,23 @@
+// JavaScript function that wraps everything
+$(document).ready(function() {
+
+var myQuestions = [
+  {
+    question: "What famous beach is known for being in the final scene of point break?",
+    answers: ["Bells Beach","Bedourie","Byron Bay","Beachlands"],
+    correctAnswer: "0"
+  },
+  {
+    question: "Which of the following is not an Australian innovation?",
+    answers: ["Chiko Roll","Lawn Sprinkler","Plastic bank notes","Wine Cask"],
+    correctAnswer: "1"
+  },
+  {
+    question: "What creature does Jana Pittman have tattooed on her body?",
+    answers: [ "Scorpion", "Bee", "Dragon", "Owl" ],
+    correctAnswer: "1"
+  }
+];
+
+
+});
